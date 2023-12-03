b99cdc2c7db7c25126aa24aab487672dc37e4971
Ismail <shaikhnagismail@gmail.com>
2023-12-03 22:10:35 +0200

    Convert image format from jpg to png and generate webp files

--- public/images/video-posters/convert-images.sh
+++ public/images/video-posters/convert-images.sh
@@ -2,6 +2,6 @@
 
 mkdir -p webp
 
-for file in *.jpg; do
-  ffmpeg -i "$file" -pix_fmt yuv420p "webp/$(basename "$file" .jpg).webp"
+for file in *.png; do
+  ffmpeg -i "$file" -pix_fmt yuv420p "webp/$(basename "$file" .png).webp"
 done
