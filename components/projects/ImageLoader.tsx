46230a9b3499936eac0732fe7f9b4c05106039b1
Ismail <shaikhnagismail@gmail.com>
2023-12-11 03:16:22 +0200

    refactor: remove the error tracking functionality

--- components/projects/ImageLoader.tsx
+++ components/projects/ImageLoader.tsx
@@ -12,17 +12,11 @@ interface ImageLoaderProps
 
 const ImageLoader = ({ src, alt, ...props }: ImageLoaderProps) => {
   const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false); // new state for error tracking
 
   const handleImageLoad = () => {
     setIsLoading(false);
-    setHasError(false); // reset error state on successful load
   };
 
-  const handleImageError = () => {
-    setIsLoading(false);
-    setHasError(true); // set error state on image load failure
-  };
 
   return (
     <>
@@ -33,7 +27,6 @@ const ImageLoader = ({ src, alt, ...props }: ImageLoaderProps) => {
           src={src} // use fallback image if main image fails to load
           alt={alt || "image"}
           onLoad={handleImageLoad}
-          onError={handleImageError} // handle image load failure
           {...props}
         />
       )}
